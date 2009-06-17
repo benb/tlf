0225130e06e1d54f30d49544b2ce69c5b3fb2a49
Ben Blackburne <bpb@lysozyme.net>
2009-06-17 11:05:42 +0100

    Add code to publish to maven repo

--- project/build/src/Tlf.scala
+++ project/build/src/Tlf.scala
@@ -3,6 +3,8 @@ import sbt._
 class TLFProject(info: ProjectInfo) extends DefaultProject(info)
 {
   val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5"
+  override def managedStyle = ManagedStyle.Maven 
+  lazy val publishTo = Resolver.file("Publish", new java.io.File("../benb.github.com/maven/ ")) 
 }
 
 
