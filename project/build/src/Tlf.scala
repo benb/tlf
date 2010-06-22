c52033adde56b9fae64b43b98ac205f2079d5a9c
Ben Blackburne <bpb@lysozyme.net>
2010-06-22 16:00:17 +0100

    => RC6

--- project/build/src/Tlf.scala
+++ project/build/src/Tlf.scala
@@ -3,7 +3,7 @@ import sbt._
 class TLFProject(info: ProjectInfo) extends DefaultProject(info)
 {
   val newReleaseToolsRepository = "Scala Tools Repository" at " http://nexus.scala-tools.org/content/repositories/snapshots/" 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" 
+  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" 
   override def managedStyle = ManagedStyle.Maven 
   lazy val publishTo = Resolver.file("Publish", new java.io.File("../benb.github.com/maven/")) 
 }
